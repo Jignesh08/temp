972356daec03b960277afcdcd14e5073577e6d17
Jignesh <jigskubavat2000@gmail.com>
2021-06-06 16:03:29 +0530

    Add Console log

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -5,6 +5,7 @@ import {Feather} from '@expo/vector-icons';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const IndexScreen = ({navigation}) => {
+    console.log("Hello user");
     const {state, deleteBlogPost, getBlogPost} = useContext(Context);
 
     useEffect(() => {
